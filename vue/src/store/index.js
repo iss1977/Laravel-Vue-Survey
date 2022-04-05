a86958de36eca4f769e658272017ead15f15404e
iss1977 <ioan.szucs@gmail.com>
2022-04-05 20:05:19 +0200

    Create Layout and Navbar working

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,14 +3,24 @@ import {createStore} from 'vuex';
 const store = createStore({ //options
   state: {
     user: {
-      data: { name: 'Sebastian' },
+      data:{
+        name: 'Tom Cook',
+        email: 'tom@example.com',
+        imageUrl:
+          'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      },
       token: 123
     }
   },
 
   getters: {},
   actions: {},
-  mutations: {},
+  mutations: {
+    logout: state => {
+      state.user.data = {};
+      state.user.token = null;
+    }
+  },
   modules: {}
 })
 
